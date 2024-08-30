b92efd2cd9642e063390fc2d7584970024d69eab
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-08-30 19:31:33 +0400

    compute the test file name

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -7,7 +7,10 @@ macro_rules! log_invocation {
     () => {
         let parts: Vec<&str> = file!().split("/").collect();
         let where_to_place_the_test_file = parts[..parts.len() - 1].to_vec().join("/");
-        println!("{:?}", where_to_place_the_test_file);
+        // println!("{:?}", where_to_place_the_test_file);
+
+        let test_file = (module_path!().to_string() + ".approved.txt").to_string();
+        println!("Test file: {}", test_file);
         println!(
             "Invoked from file: {}, line: {}, module: {}",
             file!(),
