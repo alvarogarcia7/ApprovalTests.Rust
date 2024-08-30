ff476f820a65a1348a1426bb4f92c2d6f369c199
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-08-30 20:07:48 +0400

    manually parse the backtrace

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -10,12 +10,20 @@ macro_rules! log_invocation {
         // println!("{:?}", where_to_place_the_test_file);
         let backtraceX = std::backtrace::Backtrace::capture();
         let all_backtrace = format!("{:#?}", backtraceX);
-        let mut set_place = false;
+        let mut set_place = 0;
+        let mut selected = "";
         all_backtrace.split("\n").for_each(|line| {
             if line.contains(module_path!()) {
-                if !set_place {
-                    println!("Backtrace: {:?}", line);
-                    set_place = true;
+                if set_place == 0 {
+                    let parts = line.trim().split(", ").collect::<Vec<&str>>();
+                    let parts1 = parts[0].splitn(1, ":").collect::<Vec<&str>>();
+                    let parts2 = parts[0].split("\"").collect::<Vec<&str>>();
+                    selected = parts2[1];
+
+                    println!("Backtrace: {:?}", parts);
+                    println!("Backtrace: {:?}", parts1);
+                    println!("Backtrace: {:?}", parts2);
+                    set_place = 1;
                 }
             }
         });
@@ -23,10 +31,11 @@ macro_rules! log_invocation {
         let test_file = (module_path!().to_string() + ".approved.txt").to_string();
         println!("Test file: {}", test_file);
         println!(
-            "Invoked from file: {}, line: {}, module: {}",
+            "Invoked from file: {}, line: {}, module: {}, function: {}",
             file!(),
             line!(),
-            module_path!()
+            module_path!(),
+            selected
         );
     };
 }
