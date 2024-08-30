6694b3d56f0a463379503766bfb32bd83444afcf
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-08-30 19:28:09 +0400

    compute where the file should go

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -2,12 +2,11 @@ pub fn verify(string: String) {
     assert_eq!(string, "Hello, world!");
 }
 
-
 #[macro_export]
 macro_rules! log_invocation {
     () => {
         let parts: Vec<&str> = file!().split("/").collect();
-        let parts2 = parts[..parts.len() - 1].to_vec();
+        let parts2 = parts[..parts.len() - 1].to_vec().join("/");
         println!("{:?}", parts2);
         println!(
             "Invoked from file: {}, line: {}, module: {}",
@@ -17,4 +16,3 @@ macro_rules! log_invocation {
         );
     };
 }
-
