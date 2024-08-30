301e07b8ff5ad080c4e65d2f433d34a21b5cbcfe
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-08-30 20:00:43 +0400

    manually parse the first matching backtrace line
    Backtrace: "    { fn: \"sample_project::tests::main_execution\", file: \"./src/main.rs\", line: 14 },"

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -8,9 +8,17 @@ macro_rules! log_invocation {
         let parts: Vec<&str> = file!().split("/").collect();
         let where_to_place_the_test_file = parts[..parts.len() - 1].to_vec().join("/");
         // println!("{:?}", where_to_place_the_test_file);
-        let backtrace = std::backtrace::Backtrace::capture();
-        let all_backtrace = format!("{:?}", backtrace);
-        eprintln!("My backtrace: {:#?}", backtrace);
+        let backtraceX = std::backtrace::Backtrace::capture();
+        let all_backtrace = format!("{:#?}", backtraceX);
+        let mut set_place = false;
+        all_backtrace.split("\n").for_each(|line| {
+            if line.contains(module_path!()) {
+                if !set_place {
+                    println!("Backtrace: {:?}", line);
+                    set_place = true;
+                }
+            }
+        });
 
         let test_file = (module_path!().to_string() + ".approved.txt").to_string();
         println!("Test file: {}", test_file);
